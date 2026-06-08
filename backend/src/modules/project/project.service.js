e6732baaeefd6ff986c61d0b379347f4864e6887
Lucas <lucassousa180810@gmail.com>
2026-06-08 02:44:43 -0300

    feat: as infos do dono do projeto na tabela relacional são agora criados junto com as informações do projeto geral em uma transaçao

--- backend/src/modules/project/project.service.js
+++ backend/src/modules/project/project.service.js
@@ -1,11 +1,21 @@
 import prisma from "../../../database/prisma/prisma.ts"
 
 export const createProject = async ({userId, name, description = null}) => {
-    return await prisma.projects.create({
-        data: {
-            owner_id: userId,
-            name: name,
-            description: description,
-        }
+    return await prisma.$transaction(async (tx) => {
+        const project = await tx.projects.create({
+            data: {
+                owner_id: userId,
+                name: name,
+                description: description,
+            }
+        })
+        const adminRelation = await tx.users_projects.create({
+            data: {
+                project_id: project.id,
+                user_id: userId,
+                role: 'owner',
+            }
+        })
+        return {project, adminRelation};
     })
 }
